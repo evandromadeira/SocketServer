738d0003d5a25470e6b71a6739b2ed34561d482a
evandromadeira <evandromfmadeira@gmail.com>
2018-11-23 00:00:57 -0200

    SocketServer
    SocketServer em C# trocando informações na mesma máquina

---
+++ SocketServer/Server.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SocketServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbConversa = new System.Windows.Forms.RichTextBox();
+            this.rtbMensagem = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbConversa
+            // 
+            this.rtbConversa.Location = new System.Drawing.Point(12, 12);
+            this.rtbConversa.Name = "rtbConversa";
+            this.rtbConversa.Size = new System.Drawing.Size(470, 300);
+            this.rtbConversa.TabIndex = 0;
+            this.rtbConversa.Text = "";
+            // 
+            // rtbMensagem
+            // 
+            this.rtbMensagem.Location = new System.Drawing.Point(12, 318);
+            this.rtbMensagem.Name = "rtbMensagem";
+            this.rtbMensagem.Size = new System.Drawing.Size(470, 96);
+            this.rtbMensagem.TabIndex = 1;
+            this.rtbMensagem.Text = "";
+            this.rtbMensagem.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RtbMensagem_KeyDown);
+            this.rtbMensagem.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RtbMensagem_KeyUp);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(494, 426);
+            this.Controls.Add(this.rtbMensagem);
+            this.Controls.Add(this.rtbConversa);
+            this.Name = "Server";
+            this.Text = "UNESC - Redes de computadores - Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbConversa;
+        private System.Windows.Forms.RichTextBox rtbMensagem;
+    }
+}
+
---
+++ SocketServer/Server.cs
@@ -0,0 +1,199 @@
+﻿using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System;
+
+namespace SocketServer
+{
+    public partial class Server : Form
+    {
+        int numeroPorta = 8000;
+        TcpListener tcpListener;
+        TcpClient tcpClient;
+        NetworkStream networkStream;
+        Thread thInteraction;
+
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private bool Connect()
+        {
+            bool retorno = false;
+
+            try
+            {
+                tcpListener = new TcpListener(System.Net.IPAddress.Any, numeroPorta);
+                tcpListener.Start();
+                retorno = true;
+            }
+            catch
+            {
+
+            }
+
+            return retorno;
+        }
+
+        private void Disconnect()
+        {
+            if (thInteraction != null)
+            {
+                if (thInteraction.ThreadState == ThreadState.Running)
+                {
+                    thInteraction.Abort();
+                }
+            }
+
+            if (tcpClient != null)
+            {
+                tcpClient.Client.Disconnect(true);
+            }
+
+            tcpListener.Stop();
+
+            SetMsg("## Conexões perdidas...", true);
+        }
+
+        private void AcceptConnection()
+        {
+            try
+            {
+                tcpClient = tcpListener.AcceptTcpClient();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void EnviarMsg(string mensagem)
+        {
+            if (PodeEscrever())
+            {
+                byte[] sendBytes = Encoding.ASCII.GetBytes(mensagem);
+                networkStream.Write(sendBytes, 0, sendBytes.Length);
+            }
+        }
+
+        private bool PodeEscrever()
+        {
+            return (networkStream == null || !(networkStream.CanWrite && tcpClient != null)) ? false : true;
+        }
+
+        delegate void delSetMsg(string mensagem, bool burlar);
+        private void SetMsg(string mensagem, bool burlar)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new delSetMsg(SetMsg), mensagem, burlar);
+            }
+            else
+            {
+                if (burlar || PodeEscrever())
+                {
+                    rtbConversa.Text += "\nEu: " + mensagem;
+                }
+            }
+        }
+
+        delegate void delGetMsg(string mensagem);
+        private void GetMsg(string mensagem)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new delGetMsg(GetMsg), mensagem);
+            }
+            else
+            {
+                if (PodeEscrever())
+                {
+                    rtbConversa.Text += "\nClient: " + mensagem;
+                }
+            }
+        }
+
+        private void Start()
+        {
+            if (Connect())
+            {
+                SetMsg("## Aguardando uma conexão...", true);
+            }
+
+            thInteraction = new Thread(new ThreadStart(Interaction));
+            thInteraction.IsBackground = true;
+            thInteraction.Priority = ThreadPriority.Highest;
+            thInteraction.Name = "thInteraction";
+            thInteraction.Start();
+        }
+
+        private void Interaction()
+        {
+            try
+            {
+                AcceptConnection();
+                SetMsg("## Conexão aceita...", true);
+
+                do
+                {
+                    networkStream = tcpClient.GetStream();
+
+                    if (networkStream.CanRead)
+                    {
+                        byte[] bytes = new byte[tcpClient.ReceiveBufferSize];
+                        networkStream.Read(bytes, 0, Convert.ToInt32(tcpClient.ReceiveBufferSize));
+
+                        string clientData = Encoding.ASCII.GetString(bytes);
+
+                        if (!clientData.Replace("\0", "").Trim().Equals(""))
+                        {
+                            GetMsg(clientData);
+                        }
+                        else
+                        {
+                            tcpClient = null;
+                        }
+                    }
+                } while (tcpClient != null);
+
+                Disconnect();
+                Start();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SetMsg("## Encerrando conexão com o servidor...", true);
+            Disconnect();
+        }
+
+        private void RtbMensagem_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string mensagem = rtbMensagem.Text;
+                EnviarMsg(mensagem);
+                SetMsg(mensagem, false);
+            }
+        }
+
+        private void RtbMensagem_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                rtbMensagem.Clear();
+            }
+        }
+    }
+}
